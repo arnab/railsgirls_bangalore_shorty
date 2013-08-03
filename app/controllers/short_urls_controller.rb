a0c561b80adc1f7322e0694dc6bcc8a962940d24
Arnab Deka <arnab.deka@gmail.com>
2013-08-03 16:22:30 +0530

    bootstrapped flash

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -1,6 +1,7 @@
 class ShortUrlsController < ApplicationController
   def create
     ShortUrl.create(url: params[:url])
+    flash[:notice] = "Successfully create short URL "
     redirect_to root_url
   end
 
