50ddb7585d3cf400bb8d868e4b150822ed8edc66
Arnab Deka <arnab.deka@gmail.com>
2013-08-03 16:54:08 +0530

    generate short code

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -1,7 +1,7 @@
 class ShortUrlsController < ApplicationController
   def create
-    ShortUrl.create(url: params[:url])
-    flash[:notice] = "Successfully create short URL "
+    short_url = ShortUrl.create(url: params[:url])
+    flash[:notice] = "Successfully create short URL: #{short_url.short_code} "
     redirect_to root_url
   end
 
--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -1,2 +1,7 @@
 class ShortUrl < ActiveRecord::Base
+  before_create :generate_short_code
+
+  def generate_short_code
+    self.short_code = SecureRandom.uuid.chars.last(5).join
+  end
 end
